549209964b0e3e15a24a40779eadd2ef7767cbe2
Pulkit Garg <pulkitgarg784@gmail.com>
2021-03-19 22:20:21 +0530

    fixed wrong instantiation

--- Assets/PlaceCube.cs
+++ Assets/PlaceCube.cs
@@ -30,13 +30,12 @@ public class PlaceCube : MonoBehaviour
             RaycastHit hit;
             if(Physics.Raycast(ray,out hit,1000f)) {
                 wordPos=hit.point;
-            } else {
-                wordPos=mainCamera.ScreenToWorldPoint(mousePos);
             }
             wordPos.x = Mathf.Round((wordPos.x / gridSize) *gridSize);
             wordPos.y = Mathf.Round((wordPos.y / gridSize) *gridSize);
             wordPos.z = Mathf.Round((wordPos.z / gridSize) *gridSize);
             Debug.Log(wordPos);
+            
             GameObject go =  Instantiate(cube,wordPos,Quaternion.identity); 
             go.transform.SetParent(parent);
             go.GetComponent<Renderer>().material.color = picker.CurrentColor;
