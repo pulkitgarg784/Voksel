f4eb04dc18fa72654cf9f8c5bf7a7bb900736e96
Pulkit Garg <pulkitgarg784@gmail.com>
2021-04-11 20:38:16 +0530

    basic mesh combining
    https://github.com/dawid-t/Mesh-Combiner

---
+++ Assets/CombineMeshes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineMeshes : MonoBehaviour
+{
+    private MeshCombiner meshCombiner;
+    private void Start()
+    {
+        meshCombiner = gameObject.AddComponent<MeshCombiner>();
+        meshCombiner.CreateMultiMaterialMesh = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            meshCombiner.CombineMeshes(false);
+        }
+    }
+}
