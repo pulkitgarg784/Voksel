9b271b649841de45ee8d0114e024f96ad308fb3b
Pulkit Garg <pulkitgarg784@gmail.com>
2021-04-27 11:14:25 +0530

    Added Draggable Windows

---
+++ Assets/Scripts/UI/DragWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragWindow : MonoBehaviour,IDragHandler,IPointerDownHandler
+{
+    private RectTransform dragRectTransform;
+    [SerializeField] private Canvas parentCanvas;
+
+    private void Awake()
+    {
+        dragRectTransform = transform.GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta/parentCanvas.scaleFactor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragRectTransform.SetAsLastSibling();
+    }
+}
